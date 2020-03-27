e5480d029b8122a4e826d35a2680935c02e38928
Montazio <30464013+Montazio@users.noreply.github.com>
2020-03-27 12:33:37 +0100

    ON:patientFormModal; Added timer | Code refractoring

--- src/@fuse/components/patientFormModal/patientFormModal.js
+++ src/@fuse/components/patientFormModal/patientFormModal.js
@@ -1,40 +1,24 @@
 import * as React from "react";
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 import { Modal } from "@fuse";
 import { Formik, Form, Field } from "formik";
 import { withRouter } from "react-router-dom";
 import QuestionEducation from "./QuestionEducation";
-import {
-  Button
-  /*   LinearProgress,
-  MenuItem,
-  FormControl,
-  InputLabel,
-  FormControlLabel */
-} from "@material-ui/core";
-import MuiTextField from "@material-ui/core/TextField";
+import { Button, MuiTextField } from "@material-ui/core";
+// import MuiTextField from "@material-ui/core/TextField";
 import {
   fieldToTextField,
   TextField,
   TextFieldProps
-  /*   Select,
-  Switch */
 } from "formik-material-ui";
-/* import {
-  TimePicker,
-  DatePicker,
-  DateTimePicker
-} from "formik-material-ui-pickers"; */
 import { MuiPickersUtilsProvider } from "@material-ui/pickers";
 import DateFnsUtils from "@date-io/date-fns";
-import { Recorder } from 'react-voice-recorder'
-import MicRecorder from 'mic-recorder-to-mp3';
+import MicRecorder from "mic-recorder-to-mp3";
 import IconButton from "@material-ui/core/IconButton";
 import MicIcon from "@material-ui/icons/Mic";
 const Mp3Recorder = new MicRecorder({ bitRate: 128 });
-
 
 const PatientFormModal = ({
   staticCount,
@@ -44,26 +28,22 @@ const PatientFormModal = ({
   submitFormCallback,
   updateResponse
 }) => {
-
   const [isRecording, setIsRecording] = useState(false);
-  const [blobURL, setBlobURL] = useState('');
+  const [blobURL, setBlobURL] = useState("");
   const [isBlocked, setIsBlocked] = useState(false);
-  const [paly, setplay] = useState(true);
+  const [play, setplay] = useState(true);
   const [stopRecord, setstopRecord] = useState(false);
-  const [base64Image, setbase64Image] = useState('');
-  const [isActive, setActive] = useState(false);
-
-
+  const [base64Audio, setbase64Audio] = useState("");
 
   useEffect(() => {
     navigator.getUserMedia(
       { audio: true },
       () => {
-        console.log('Permission Granted');
+        console.log("Permission Granted");
         setIsBlocked(false);
       },
       () => {
-        console.log('Permission Denied');
+        console.log("Permission Denied");
         setIsBlocked(true);
       }
     );
@@ -75,73 +55,71 @@ const PatientFormModal = ({
         const blobURL = URL.createObjectURL(blob);
         setIsRecording(false);
         setBlobURL(blobURL);
-        setplay(true)
-        setstopRecord(false)
+        setplay(true);
+        setstopRecord(false);
         axios({
-          method: 'get',
+          method: "get",
           url: blobURL,
-          responseType: 'blob'
-        }).then(function (response) {
-
+          responseType: "blob"
+        }).then(function(response) {
           var reader = new FileReader();
           reader.readAsDataURL(response.data);
-          reader.onloadend = function () {
+          reader.onloadend = function() {
             var base64data = reader.result;
             base64data = base64data.split(",")[1];
-            setbase64Image(base64data)
-          }
-
-        })
-
+            setbase64Audio(base64data);
+          };
+        });
       })
       .catch(e => console.log(e));
   };
 
-
   const start = () => {
     if (isBlocked) {
-      console.log('Permission Denied');
+      console.log("Permission Denied");
     } else {
       Mp3Recorder.start()
         .then(() => {
           setIsRecording(true);
-          setplay(false)
-          setstopRecord(true)
+          setplay(false);
+          setstopRecord(true);
         })
         .catch(e => console.error(e));
-
     }
   };
+  // TIMER START
+  const [mSeconds, setMSeconds] = useState(0);
+  const [isActive, setIsActive] = useState(false);
 
+  function toggle() {
+    setIsActive(!isActive);
+  }
+
+  function reset() {
+    setMSeconds(0);
+    setIsActive(false);
+  }
+  useEffect(() => {
+    let interval = null;
+    if (isActive && mSeconds < 30) {
+      interval = setInterval(() => {
+        setMSeconds(mSeconds => mSeconds + 1);
+      }, 1000);
+    } else if (!isActive && mSeconds !== 0) {
+      clearInterval(interval);
+    }
+    if (mSeconds >= 30) {
+      clearInterval(interval);
+      stop();
+      reset();
+    }
+    return () => clearInterval(interval);
+  }, [isActive, mSeconds]);
 
   const handleClose = id => {
     modalAction(id);
   };
 
-
-  /*   function UpperCasingTextField(props) {
-      const {
-        form: { setFieldValue },
-        field: { name }
-      } = props;
-      const onChange = React.useCallback(
-        event => {
-          const { value } = event.target;
-          setFieldValue(name, value ? value.toUpperCase() : "");
-        },
-        [setFieldValue, name]
-      );
-      return (
-        <MuiTextField
-          {...fieldToTextField({
-            label: "Outlined",
-            variant: "outlined",
-            ...props
-          })}
-          onChange={onChange}
-        />
-      );
-    } */
   const getAllState = data => {
     updateResponse(data);
   };
@@ -173,26 +151,34 @@ const PatientFormModal = ({
             );
           })}
         <h4 className="personnal-question-title">MESSAGE VOCAL</h4>
+        <label className="small">
+          Vous avez 30 secondes pour décrire votre état et pour qu'on puisse
+          mieux vous diagnostiquer
+        </label>
         <div className="tim3">
-          <div >
-            {
-              paly == true ? <IconButton   color="primary" aria-label="record" onClick={start} onClick={() => {
-                start()
-                setTimeout(function () { stop() }, 3000);
+          <div style={{ textAlign: "center" }}>
+            {play ? "Cliquez ICI" : mSeconds + "/30"}
+            <IconButton
+              color={play ? "primary" : "secondary"}
+              aria-label="record"
+              onClick={() => {
+                if (play) {
+                  start();
+                } else {
+                  stop();
+                }
+                toggle();
               }}
-                disabled={isRecording} >  <MicIcon /></IconButton> : ""}
-            {
-              stopRecord == true ? <IconButton  color="secondary" aria-label="record"  
-              onClick={stop} disabled={!isRecording} > <MicIcon /></IconButton> : ""
-            }
+              disabled={stopRecord ? !isRecording : isRecording}
+            >
+              <MicIcon />
+            </IconButton>
             <br></br>
             <div className="tim4">
               <audio src={blobURL} controls="controls" />
-
             </div>
           </div>
         </div>
-
 
         <h4 className="personnal-question-title">Données Personnelles</h4>
         <Formik
@@ -234,7 +220,6 @@ const PatientFormModal = ({
               errors.mytel = "invalid length";
             }
 
-
             if (values.zipcode === "") {
               errors.zipcode = "Required";
             } else if (values.zipcode.length <= 3) {
@@ -250,7 +235,7 @@ const PatientFormModal = ({
               address: values.adresse,
               zipCode: values.zipcode,
               phoneNumber: values.mytel,
-              audio:base64Image
+              audio: base64Audio
             };
             submitFormCallback(caste);
           }}
@@ -261,52 +246,52 @@ const PatientFormModal = ({
             values,
             setFieldValue
           }) => (
-              <MuiPickersUtilsProvider utils={DateFnsUtils}>
-                <Form>
-                  <div
+            <MuiPickersUtilsProvider utils={DateFnsUtils}>
+              <Form>
+                <div
+                  style={{
+                    margin: 10
+                  }}
+                >
+                  <Field
+                    component={TextField}
+                    type="text"
+                    label="Nom"
+                    name="nom"
+                    variant="outlined"
                     style={{
-                      margin: 10
+                      margin: "0 12px"
                     }}
-                  >
-                    <Field
-                      component={TextField}
-                      type="text"
-                      label="Nom"
-                      name="nom"
-                      variant="outlined"
-                      style={{
-                        margin: "0 12px"
-                      }}
-                    />
-                    <Field
-                      component={TextField}
-                      type="text"
-                      label="Prenom"
-                      name="prenom"
-                      variant="outlined"
-                      style={{
-                        margin: "0 12px"
-                      }}
-                    />
-                  </div>
-                  <div
+                  />
+                  <Field
+                    component={TextField}
+                    type="text"
+                    label="Prenom"
+                    name="prenom"
+                    variant="outlined"
                     style={{
-                      margin: 10
+                      margin: "0 12px"
                     }}
-                  >
-                    <Field
-                      component={TextField}
-                      type="text"
-                      label="Adress"
-                      name="adresse"
-                      variant="outlined"
-                      style={{
-                        margin: "0 12px"
-                      }}
-                    />
-                  </div>
+                  />
+                </div>
+                <div
+                  style={{
+                    margin: 10
+                  }}
+                >
+                  <Field
+                    component={TextField}
+                    type="text"
+                    label="Adress"
+                    name="adresse"
+                    variant="outlined"
+                    style={{
+                      margin: "0 12px"
+                    }}
+                  />
+                </div>
 
-                  {/*  <Field
+                {/*  <Field
                     component={TextField}
                     type="text"
                     label="Cin"
@@ -316,23 +301,23 @@ const PatientFormModal = ({
                       margin: "0 12px"
                     }}
                   /> */}
-                  <div
+                <div
+                  style={{
+                    margin: 10
+                  }}
+                >
+                  <Field
+                    component={TextField}
+                    type="text"
+                    label="Numero de telephone"
+                    name="mytel"
+                    variant="outlined"
                     style={{
-                      margin: 10
+                      margin: "0 12px"
                     }}
-                  >
-                    <Field
-                      component={TextField}
-                      type="text"
-                      label="Numero de telephone"
-                      name="mytel"
-                      variant="outlined"
-                      style={{
-                        margin: "0 12px"
-                      }}
-                    />
+                  />
 
-                    {/* <Field
+                  {/* <Field
                     component={UpperCasingTextField}
                     name="email"
                     type="email"
@@ -341,18 +326,18 @@ const PatientFormModal = ({
                       margin: "0 12px"
                     }}
                   /> */}
-                    <Field
-                      component={TextField}
-                      type="text"
-                      label="Zip Code"
-                      name="zipcode"
-                      variant="outlined"
-                      style={{
-                        margin: "0 12px"
-                      }}
-                    />
-                  </div>
-                  {/*                <div
+                  <Field
+                    component={TextField}
+                    type="text"
+                    label="Zip Code"
+                    name="zipcode"
+                    variant="outlined"
+                    style={{
+                      margin: "0 12px"
+                    }}
+                  />
+                </div>
+                {/*                <div
                   style={{
                     margin: 10
                   }}
@@ -368,38 +353,38 @@ const PatientFormModal = ({
                     }}
                   />
                 </div> */}
-                  <div className="action-buttons">
-                    <Button
-                      className="cancel"
-                      variant="outlined"
-                      color="primary"
-                      disabled={isSubmitting}
-                      onClick={() => {
-                        resetForm();
-                        handleClose("PatientForm");
-                      }}
-                    >
-                      Annuler
+                <div className="action-buttons">
+                  <Button
+                    className="cancel"
+                    variant="outlined"
+                    color="primary"
+                    disabled={isSubmitting}
+                    onClick={() => {
+                      resetForm();
+                      handleClose("PatientForm");
+                    }}
+                  >
+                    Annuler
                   </Button>
-                    <Button
-                      className="submit"
-                      variant="contained"
-                      color="primary"
-                      disabled={isSubmitting}
-                      onClick={() => {
-                        if (staticCount === dynamicCount) {
-                          submitForm();
-                        } else {
-                          alert("Merci de répondre à toutes les questions");
-                        }
-                      }}
-                    >
-                      Valider
+                  <Button
+                    className="submit"
+                    variant="contained"
+                    color="primary"
+                    disabled={isSubmitting}
+                    onClick={() => {
+                      if (staticCount === dynamicCount) {
+                        submitForm();
+                      } else {
+                        alert("Merci de répondre à toutes les questions");
+                      }
+                    }}
+                  >
+                    Valider
                   </Button>
-                  </div>
-                </Form>
-              </MuiPickersUtilsProvider>
-            )}
+                </div>
+              </Form>
+            </MuiPickersUtilsProvider>
+          )}
         />
       </div>
     </Modal>
